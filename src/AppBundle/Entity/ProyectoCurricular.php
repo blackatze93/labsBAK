63d0d71e04303010555ace6d0d05efc73569b6bd
Andres <afsanchez93@gmail.com>
2016-11-09 17:02:13 -0500

    Generando la entidad proyecto curricular

---
+++ src/AppBundle/Entity/ProyectoCurricular.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class ProyectoCurricular
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column()
+     */
+    protected $codigo;
+
+    /**
+     * @ORM\Column()
+     */
+    protected $nombre;
+
+    /**
+     * @return mixed
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * @param mixed $codigo
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function __toString()
+    {
+        return $this->getNombre();
+    }
+
+}
