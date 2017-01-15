876fa49ef6bb9ae7d9a43130e69c4dc057790891
Andres Sanchez <afsanchez93@gmail.com>
2017-01-15 18:55:56 -0500

    Tests

--- src/AppBundle/Tests/ApplicationAvailabilityFunctionalTest.php
+++ src/AppBundle/Tests/ApplicationAvailabilityFunctionalTest.php
@@ -3,18 +3,43 @@
 namespace AppBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class ApplicationAvailabilityFunctionalTest extends WebTestCase
 {
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        // the firewall context (defaults to the firewall name)
+        $firewall = 'secured_area';
+
+        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
     /**
     * @dataProvider urlProvider
     */
     public function testPageIsSuccessful($url)
     {
-        $client = self::createClient();
-        $client->request('GET', $url);
+        $this->logIn();
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->client->request('GET', $url);
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
     public function urlProvider()
