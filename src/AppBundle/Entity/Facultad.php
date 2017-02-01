2102226e2ba7bb1e71d846451eea0605d370f647
Andres Sanchez <afsanchez93@gmail.com>
2017-02-01 12:51:29 -0500

    Agregada entidad Facultad

---
+++ src/AppBundle/Entity/Facultad.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints as DoctrineAssert;
+
+/**
+ * Facultad
+ *
+ * @ORM\Table(name="facultad")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FacultadRepository")
+ * @DoctrineAssert\UniqueEntity("nombre")
+ */
+class Facultad
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=45, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="45")
+     */
+    private $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Facultad
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getNombre();
+    }
+}
