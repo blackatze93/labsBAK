813e58db292a94060e6b6649f74774c8bfd2f88d
Andres Sanchez <afsanchez93@gmail.com>
2017-02-04 17:36:12 +0000

    Creando entidad de Estudiante

---
+++ src/AppBundle/Entity/Estudiante.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Estudiante
+ *
+ * @ORM\Table(name="estudiante")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EstudianteRepository")
+ */
+class Estudiante
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=60)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellido", type="string", length=60)
+     */
+    private $apellido;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=100, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="estado", type="string", length=45)
+     */
+    private $estado;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="proyectocurricular", type="string", length=255)
+     */
+    private $proyectocurricular;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Estudiante
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellido
+     *
+     * @param string $apellido
+     * @return Estudiante
+     */
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    /**
+     * Get apellido
+     *
+     * @return string 
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Estudiante
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set estado
+     *
+     * @param string $estado
+     * @return Estudiante
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    /**
+     * Get estado
+     *
+     * @return string 
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * Set proyectocurricular
+     *
+     * @param string $proyectocurricular
+     * @return Estudiante
+     */
+    public function setProyectocurricular($proyectocurricular)
+    {
+        $this->proyectocurricular = $proyectocurricular;
+
+        return $this;
+    }
+
+    /**
+     * Get proyectocurricular
+     *
+     * @return string 
+     */
+    public function getProyectocurricular()
+    {
+        return $this->proyectocurricular;
+    }
+}
