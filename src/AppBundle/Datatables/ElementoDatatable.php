916cdeb99b4c092856e554dfab143c44646fc711
Andres Sanchez <afsanchez93@gmail.com>
2017-02-07 21:00:08 -0500

    Generando Datatable de Elemento

---
+++ src/AppBundle/Datatables/ElementoDatatable.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace AppBundle\Datatables;
+
+use Sg\DatatablesBundle\Datatable\View\AbstractDatatableView;
+use Sg\DatatablesBundle\Datatable\View\Style;
+
+/**
+ * Class ElementoDatatable
+ *
+ * @package AppBundle\Datatables
+ */
+class ElementoDatatable extends AbstractDatatableView
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildDatatable(array $options = array())
+    {
+        $this->topActions->set(array(
+            'start_html' => '<div class="row"><div class="col-sm-3">',
+            'end_html' => '<hr></div></div>',
+            'actions' => array(
+                array(
+                    'route' => $this->router->generate('elemento_new'),
+                    'label' => $this->translator->trans('datatables.actions.new'),
+                    'icon' => 'glyphicon glyphicon-plus',
+                    'attributes' => array(
+                        'rel' => 'tooltip',
+                        'title' => $this->translator->trans('datatables.actions.new'),
+                        'class' => 'btn btn-primary',
+                        'role' => 'button'
+                    ),
+                )
+            )
+        ));
+
+        $this->features->set(array(
+            'auto_width' => true,
+            'defer_render' => false,
+            'info' => true,
+            'jquery_ui' => false,
+            'length_change' => true,
+            'ordering' => true,
+            'paging' => true,
+            'processing' => true,
+            'scroll_x' => false,
+            'scroll_y' => '',
+            'searching' => true,
+            'state_save' => false,
+            'delay' => 0,
+            'extensions' => array(),
+            'highlight' => false,
+            'highlight_color' => 'red'
+        ));
+
+        $this->ajax->set(array(
+            'url' => $this->router->generate('elemento_results'),
+            'type' => 'GET',
+            'pipeline' => 0
+        ));
+
+        $this->options->set(array(
+            'display_start' => 0,
+            'defer_loading' => -1,
+            'dom' => 'lfrtip',
+            'length_menu' => array(10, 25, 50, 100),
+            'order_classes' => true,
+            'order' => array(array(0, 'asc')),
+            'order_multi' => true,
+            'page_length' => 10,
+            'paging_type' => Style::FULL_NUMBERS_PAGINATION,
+            'renderer' => '',
+            'scroll_collapse' => false,
+            'search_delay' => 0,
+            'state_duration' => 7200,
+            'stripe_classes' => array(),
+            'class' => Style::BOOTSTRAP_3_STYLE,
+            'individual_filtering' => false,
+            'individual_filtering_position' => 'head',
+            'use_integration_options' => true,
+            'force_dom' => false,
+            'row_id' => 'id'
+        ));
+
+        $this->columnBuilder
+            ->add('id', 'column', array(
+                'title' => 'Id',
+            ))
+            ->add('nombre', 'column', array(
+                'title' => 'Nombre',
+            ))
+            ->add('marca', 'column', array(
+                'title' => 'Marca',
+            ))
+            ->add('descripcion', 'column', array(
+                'title' => 'Descripcion',
+            ))
+            ->add('estado', 'column', array(
+                'title' => 'Estado',
+            ))
+            ->add('tipo', 'column', array(
+                'title' => 'Tipo',
+            ))
+            ->add('serial', 'column', array(
+                'title' => 'Serial',
+            ))
+            ->add('fechaIngreso', 'column', array(
+                'title' => 'FechaIngreso',
+            ))
+            ->add('tipoPrestamo', 'column', array(
+                'title' => 'TipoPrestamo',
+            ))
+            ->add('observaciones', 'column', array(
+                'title' => 'Observaciones',
+            ))
+            ->add('lugar.id', 'column', array(
+                'title' => 'Lugar Id',
+            ))
+            ->add('lugar.nombre', 'column', array(
+                'title' => 'Lugar Nombre',
+            ))
+            ->add('lugar.capacidad', 'column', array(
+                'title' => 'Lugar Capacidad',
+            ))
+            ->add('lugar.visible', 'column', array(
+                'title' => 'Lugar Visible',
+            ))
+            ->add('lugar.descripcion', 'column', array(
+                'title' => 'Lugar Descripcion',
+            ))
+            ->add(null, 'action', array(
+                'title' => $this->translator->trans('datatables.actions.title'),
+                'actions' => array(
+                    array(
+                        'route' => 'elemento_show',
+                        'route_parameters' => array(
+                            'id' => 'id'
+                        ),
+                        'label' => $this->translator->trans('datatables.actions.show'),
+                        'icon' => 'glyphicon glyphicon-eye-open',
+                        'attributes' => array(
+                            'rel' => 'tooltip',
+                            'title' => $this->translator->trans('datatables.actions.show'),
+                            'class' => 'btn btn-primary btn-xs',
+                            'role' => 'button'
+                        ),
+                    ),
+                    array(
+                        'route' => 'elemento_edit',
+                        'route_parameters' => array(
+                            'id' => 'id'
+                        ),
+                        'label' => $this->translator->trans('datatables.actions.edit'),
+                        'icon' => 'glyphicon glyphicon-edit',
+                        'attributes' => array(
+                            'rel' => 'tooltip',
+                            'title' => $this->translator->trans('datatables.actions.edit'),
+                            'class' => 'btn btn-primary btn-xs',
+                            'role' => 'button'
+                        ),
+                    )
+                )
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEntity()
+    {
+        return 'AppBundle\Entity\Elemento';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'elemento_datatable';
+    }
+}
