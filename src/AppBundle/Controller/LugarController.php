0be76f541304c30b9ea8797a7990706d9687273e
Andres <blackatze93@gmail.com>
2017-04-25 21:08:34 +0000

    Apply fixes from StyleCI

--- src/AppBundle/Controller/LugarController.php
+++ src/AppBundle/Controller/LugarController.php
@@ -2,11 +2,9 @@
 
 namespace AppBundle\Controller;
 
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Symfony\Component\HttpFoundation\Response;
 
 /**
