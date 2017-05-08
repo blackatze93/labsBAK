451d8d2f7c6ecde7f5d4adc8e6a7c8fa8b20371c
Andres Sanchez <afsanchez93@gmail.com>
2017-05-08 13:24:15 -0500

    Autocompletado con filtros

---
+++ src/AppBundle/Controller/ElementoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class ElementoController.
+ */
+class ElementoController extends BaseAdminController
+{
+    protected function autocompleteAction()
+    {
+        $results = $this->get('easyadmin.autocomplete')->find(
+            $this->request->query->get('entity'),
+            $this->request->query->get('query'),
+            $this->request->query->get('page', 1),
+            'entity.activo = true'
+        );
+
+        return new JsonResponse($results);
+    }
+}
+
