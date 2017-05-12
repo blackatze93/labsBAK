2a8740e449cb72c8ba8f2819ce198fe6fe2e4cef
Andres Sanchez <afsanchez93@gmail.com>
2017-05-12 15:08:15 -0500

    Modificando highcarts

--- src/AppBundle/Controller/GraficoController.php
+++ src/AppBundle/Controller/GraficoController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use Ob\HighchartsBundle\Highcharts\Highchart;
 /**
  * Class GraficoController.
  *
@@ -24,6 +24,41 @@ class GraficoController extends Controller
      */
     public function pruebaAction()
     {
-        return $this->render(':graficos:prueba.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $prestamos = $em->getRepository('AppBundle:PrestamoPracticaLibre')
+            ->createQueryBuilder('prestamos')
+            ->select('MONTH(prestamos.fechaPrestamo) AS fechaPrestamo')
+            ->groupBy('fechaPrestamo')
+            ->getQuery()
+            ->getResult();
+
+        var_dump($prestamos);
+
+        // Chart
+        $series = array(
+            array(
+                "name" => "Data Serie Name",
+                "data" => $prestamos
+//                "data" => array(
+////                    1,2,4,5,6,3,8
+////                    array(
+////                        "color" => '#545454',
+////                        "y" => 5
+////                    )
+//                ),
+            )
+        );
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('container');  // The #id of the div where to render the chart
+        $ob->title->text('Prestamos Practica Libre');
+        $ob->chart->type('column');
+        $ob->xAxis->title(array('text'  => "Mes"));
+        $ob->yAxis->title(array('text'  => "Cantidad"));
+        $ob->series($series);
+
+        return $this->render(':graficos:prueba.html.twig', array(
+            'chart' => $ob
+        ));
     }
 }
