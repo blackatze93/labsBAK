3b835b49d845b0342f53bd8dbf7b71aaff6e92f1
Andres Sanchez <afsanchez93@gmail.com>
2017-05-15 15:36:08 -0500

    Agregando hora inicio y fin a solicitud sala

---
+++ src/AppBundle/Controller/SolicitudSalaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\SolicitudSala;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+
+/**
+ * Class SolicitudSalaController.
+ */
+class SolicitudSalaController extends BaseAdminController
+{
+    //     * @Security("has_role('ROLE_DOCENTE') or has_role('ROLE_FUNCIONARIO')")
+
+    /**
+     * Metodo que lista los objetos encontrados en el sitio web.
+     *
+     * @Route("/solicitud_sala/", name="solicitud_sala")
+     */
+    public function solicitudSalaAction(Request $request)
+    {
+        // Se genera el formulario que permite crear el paz y salvo
+        $form = $this->createFormBuilder()
+            ->add('fecha', DateType::class, array(
+                'widget' => 'single_text',
+                'html5' => false
+            ))
+            ->add('horaInicio', TimeType::class, array(
+                'widget' => 'single_text',
+                'html5' => false
+            ))
+            ->add('horaFin', TimeType::class, array(
+                'widget' => 'single_text',
+                'html5' => false
+            ))
+            ->add('lugar', 'easyadmin_autocomplete', array(
+                'class' => 'AppBundle\Entity\Lugar'
+            ))
+            ->add('observaciones', CKEditorType::class)
+            ->add('crear', SubmitType::class)
+            ->add('restablecer', ResetType::class)
+            ->getForm()
+        ;
+
+        // Dejamos que symfony maneje el Request
+        $form->handleRequest($request);
+
+        // Si el formulario se ha enviado y es valido comprobamos el usuario
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $solicitudSala = new SolicitudSala();
+            $em = $this->getDoctrine()->getManager();
+
+            $solicitudSala->setFechaSolicitud(new \DateTime());
+            $solicitudSala->setUsuarioRealiza($this->getUser());
+            $solicitudSala->setEstado('Pendiente');
+            $solicitudSala->setFecha($data['fecha']);
+            $solicitudSala->setHoraInicio($data['horaInicio']);
+            $solicitudSala->setHoraFin($data['horaFin']);
+            $solicitudSala->setLugar($data['lugar']);
+
+            $em->persist($solicitudSala);
+            $em->flush();
+
+            return $this->render('solicitud_sala.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }
+
+        return $this->render('solicitud_sala.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
--- src/AppBundle/Entity/SolicitudSala.php
+++ src/AppBundle/Entity/SolicitudSala.php
@@ -4,14 +4,18 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints as DoctrineAssert;
+
 
 /**
- * SolicitudPrestamoSala.
+ * SolicitudSala.
  *
- * @ORM\Entity()
- * @ORM\Table(name="solicitud_prestamo_sala")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventoRepository")
+ * @ORM\Table(name="solicitud_sala")
+ * @DoctrineAssert\UniqueEntity(fields={"lugar", "fecha", "horaInicio", "horaFin"}, repositoryMethod="findRango",
+ *     message="Ya existe un evento asociado a esa fecha y lugar.", errorPath="fecha", )
  */
-class SolicitudPrestamoSala
+class SolicitudSala
 {
     /**
      * @var int
@@ -44,8 +48,7 @@ class SolicitudPrestamoSala
     /**
      * @var \DateTime
      *
-     * @ORM\Column(type="datetime")
-     * @Assert\NotBlank()
+     * @ORM\Column(type="datetime", nullable=true   )
      * @Assert\DateTime()
      */
     private $fechaRespuesta;
@@ -60,10 +63,31 @@ class SolicitudPrestamoSala
     private $fecha;
 
     /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="hora_inicio", type="time")
+     * @Assert\NotBlank()
+     * @Assert\Time()
+     */
+    private $horaInicio;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="hora_fin", type="time")
+     * @Assert\NotBlank()
+     * @Assert\Time()
+     * @Assert\Expression(
+     *     "this.getHoraInicio() < this.getHoraFin()",
+     *     message="La hora final debe ser mayor a la hora de inicio"
+     * )
+     */
+    private $horaFin;
+
+    /**
      * @var string
      *
-     * @ORM\Column(type="string", length=255, nullable=true)
-     * @Assert\Length(max="255")
+     * @ORM\Column(type="string", length=1020, nullable=true)
+     * @Assert\Length(max="1020")
      */
     private $observaciones;
 
@@ -72,7 +96,6 @@ class SolicitudPrestamoSala
      *
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Usuario")
      * @ORM\JoinColumn(name="usuario_realiza_id", referencedColumnName="id", nullable=false)
-     * @Assert\Type("AppBundle\Entity\Usuario")
      * @Assert\NotBlank()
      */
     private $usuarioRealiza;
@@ -81,11 +104,20 @@ class SolicitudPrestamoSala
      * @var Usuario
      *
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Usuario")
-     * @ORM\JoinColumn(name="usuario_responde_id", referencedColumnName="id", nullable=false)
-     * @Assert\Type("AppBundle\Entity\Usuario")
-     * @Assert\NotBlank()
+     * @ORM\JoinColumn(name="usuario_responde_id", referencedColumnName="id", nullable=true)
      */
     private $usuarioResponde;
+
+    /**
+     * @var Lugar
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Lugar")
+     * @ORM\JoinColumn(name="lugar_id", referencedColumnName="id", nullable=false, unique=false)
+     * @Assert\Type("AppBundle\Entity\Lugar")
+     * @Assert\NotBlank()
+     */
+    private $lugar;
+
 
     /**
      * @return int
@@ -168,6 +200,38 @@ class SolicitudPrestamoSala
     }
 
     /**
+     * @return \DateTime
+     */
+    public function getHoraInicio()
+    {
+        return $this->horaInicio;
+    }
+
+    /**
+     * @param \DateTime $horaInicio
+     */
+    public function setHoraInicio($horaInicio)
+    {
+        $this->horaInicio = $horaInicio;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getHoraFin()
+    {
+        return $this->horaFin;
+    }
+
+    /**
+     * @param \DateTime $horaFin
+     */
+    public function setHoraFin($horaFin)
+    {
+        $this->horaFin = $horaFin;
+    }
+
+    /**
      * @param string $observaciones
      */
     public function setObservaciones($observaciones)
@@ -205,5 +269,21 @@ class SolicitudPrestamoSala
     public function setUsuarioResponde($usuarioResponde)
     {
         $this->usuarioResponde = $usuarioResponde;
+    }
+
+    /**
+     * @return Lugar
+     */
+    public function getLugar()
+    {
+        return $this->lugar;
+    }
+
+    /**
+     * @param Lugar $lugar
+     */
+    public function setLugar($lugar)
+    {
+        $this->lugar = $lugar;
     }
 }
