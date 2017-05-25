fdacab2adbbe8d49953590360f13c2620eb5fbf1
Andres Sanchez <afsanchez93@gmail.com>
2017-05-25 02:06:54 -0500

    Agregando activo en usuario

--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -163,6 +163,7 @@ class Usuario implements AdvancedUserInterface
     public function __construct()
     {
         $this->fechaCreacion = new \DateTime();
+        $this->activo = true;
     }
 
     /**
